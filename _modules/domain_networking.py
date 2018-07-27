9b25f0a26ba2232d3d960e4f8f38a2ad42773fbe
Julian Schuh <julez@julez.in>
2018-07-27 16:47:45 +0200

    work on domain bridge interface

--- _modules/domain_networking.py
+++ _modules/domain_networking.py
@@ -2,3 +2,15 @@ def generate_ifname(domain, iftype=None):
   if iftype is None:
   	return 'dom{0:02d}'.format(domain['domain_id'])
   return 'dom{0:02d}-{1:s}'.format(domain['domain_id'], iftype)
+
+def generate_mac(type, domain, host_id):
+  if type == 'br':
+    return ":".join([
+    	'fc',
+    	'cb',
+    	'ff',
+    	'00',
+    	'{0:02d}'.format(domain['domain_id']),
+    	'{0:02d}'.format(host_id)])
+  else:
+    return "INVALID"
