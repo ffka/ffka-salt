d8a38f4ca18319f5f569138fb33c4c3ab7b4a19c
Julian Schuh <julez@julez.in>
2018-07-28 15:51:21 +0200

    imrpve ifname generation for long ifnames

--- _modules/domain_networking.py
+++ _modules/domain_networking.py
@@ -3,7 +3,7 @@ def generate_ifname(domain, iftype=None, suffix=None):
     return 'dom{0:02d}'.format(domain['domain_id'])
   if suffix is None:
     return 'dom{0:02d}-{1:s}'.format(domain['domain_id'], iftype)
-  return 'dom{0:02d}-{1:s}-{2:s}'.format(domain['domain_id'], iftype, suffix)
+  return 'dom{0:02d}-{1:s}-{2:s}'.format(domain['domain_id'], iftype, suffix[:3])
 
 def generate_mac(type, domain, host_id, instance_id=None):
   if type == 'br':
